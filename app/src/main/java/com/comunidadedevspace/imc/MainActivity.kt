5bdb0f4754149d1dcec678fe52ab5da834178bc1
Kelvin Amaral <kelvinprime42@gmail.com>
2025-03-06 23:24:22 -0200

    calculadora imc edittext

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,35 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Recuperar os componentes EditText
+        // Criar uma variavel e associar (=) o componente de ui<EditText>
+        // Recuperar o botão da tela
+
+        // Colocar acao no botao setOnLickListener
+        // Recuperar o texto digitado no edt peso
+
+        val  edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        val Btncalcular = findViewById<Button>(R.id.btn_calcular)
+
+        Btncalcular.setOnClickListener {
+            val peso: Float= edtPeso.text.toString().toFloat()
+            val altura: Float = edtAltura.text.toString().toFloat()
+
+            val alturaq2 = altura * altura
+            val resultado = peso / alturaq2
+            println("Kelvin acao do botao " + resultado)
+        }
+
+
     }
 }

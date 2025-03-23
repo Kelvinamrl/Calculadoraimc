d93df787a3bb5638ade6f075696feec0f31a609e
Kelvin Amaral <kelvinprime42@gmail.com>
2025-03-23 12:54:09 -0300

    melhoria na UI

--- app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -1,6 +1,7 @@
 package com.comunidadedevspace.imc
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,47 +17,61 @@ class ResultActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_result)
 
-        // Ajusta os paddings para respeitar as barras do sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets // Retorno correto para evitar erro de tipo
+            insets
         }
 
-        // Recupera o resultado enviado pela Intent
         val result = intent.getFloatExtra(KEY_RESULT_IMC, 0.0f)
 
         val tvResult = findViewById<TextView>(R.id.tv_result)
+        val tvMensagem = findViewById<TextView>(R.id.tv_mensagem)
         val tvClassificacao = findViewById<TextView>(R.id.tv_classificacao)
         tvResult.text = result.toString()
 
         var classificacao: String? = null
+        var mensagem: String? = null
 
         if (result <= 18.5f) {
-            classificacao = "MAGREZA"
-        } else if (result > 18.5f && result <= 24.9f) {
+            classificacao = "ABAIXO DO PESO"
+            mensagem = "Seu IMC indica abaixo do peso. Que tal consultar um profissional para avaliar sua saúde?"
+        } else if (result > 18.6f && result <= 24.9f) {
             classificacao = "NORMAL"
+            mensagem = "Parabéns! Seu IMC está normal, continue cuidando da sua saúde!"
         } else if (result > 25f && result <= 29.9f) {
             classificacao = "SOBREPESO"
-        } else if (result > 33f && result <= 39.9f) {
-            classificacao = "OBESIDADE"
+            mensagem = "Seu IMC indica sobrepeso. Uma rotina equilibrada pode ajudar a melhorar!"
+        } else if (result > 30f && result <= 39.9f) {
+            classificacao = "OBESIDADE I"
+            mensagem = "Seu IMC indica obesidade grau I. Considere buscar orientação médica para um plano saudável."
         } else {
-            classificacao = "OBESIDADE MÓRBIDA"
+            classificacao = "OBESIDADE II"
+            mensagem = "Seu IMC indica obesidade grau II. É importante buscar apoio médico para cuidar de você."
         }
 
         tvClassificacao.text = classificacao
+        tvMensagem.text = mensagem
 
         val cor = when (classificacao) {
-            "MAGREZA" -> R.color.red
+            "ABAIXO DO PESO" -> R.color.blue
             "NORMAL" -> R.color.green
             "SOBREPESO" -> R.color.yellow
+            "OBESIDADE I" -> R.color.orange
             else -> R.color.red
         }
-
         tvClassificacao.setTextColor(ContextCompat.getColor(this, cor))
 
+        val imagem = when (classificacao) {
+            "ABAIXO DO PESO" -> R.drawable.abaixo
+            "NORMAL" -> R.drawable.normal
+            "SOBREPESO" -> R.drawable.acima
+            "OBESIDADE I" -> R.drawable.obesidade
+            else -> R.drawable.obesidade_2
+        }
 
-
+        val imagemView = findViewById<ImageView>(R.id.resultado_imagem)
+        imagemView.setImageResource(imagem)
 
 
     }
